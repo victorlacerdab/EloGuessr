1dd37b629456bf20623a5ef83b2891ad75abe66e
Victor Lacerda <victorlbotelho@gmail.com>
2024-07-22 23:55:23 +0200

    Added the Linear Decoder.

--- notebooks/eloguessr.py
+++ notebooks/eloguessr.py
@@ -11,17 +11,18 @@ class EloGuessr(nn.Module):
                                       padding_idx = padding_idx)
         self.posenc = PositionalEncoding(emb_dim=embdim, max_len=max_match_len)
         self.enc_block = EncoderBlock(embdim=embdim, num_heads=num_heads, dim_ff=dim_ff, device=device)
-        #self.dec_block = LinearDecoder(embdim=embdim, device=device)
-        self.dec_block = TransfDec(embdim=embdim, num_heads=num_heads, dim_ff=dim_ff, device=device)
-        self.conv1d = nn.Conv1d(in_channels=max_match_len, out_channels=1, kernel_size=21, stride=1, padding=4)
-        self.fc_out = nn.Linear(1012, 1)
+        self.dec_block = LinearDecoder(embdim=embdim, device=device)
+        #self.dec_block = TransfDec(embdim=embdim, num_heads=num_heads, dim_ff=dim_ff, device=device)
+        self.conv1d = nn.Conv1d(in_channels=max_match_len, out_channels=1, kernel_size=5, stride=1, padding=2)
+        self.fc_out = nn.Linear(embdim, 1)
 
     def forward(self, x):
         out = self.emb_layer(x)
         out = self.posenc(out)
         out = self.enc_block(out)
-        out = self.dec_block(out, out)[:, :, :] # We pick the '[ELO]' token appended at the end as the feature vector
-        out = self.conv1d(out).squeeze()
+        # out = self.dec_block(out, out)[:, :, :] # We pick the '[ELO]' token appended at the end as the feature vector
+        out = F.relu(self.conv1d(out).squeeze())
+        out = self.dec_block(out)
         out = self.fc_out(out)
         return out
     
@@ -36,10 +37,20 @@ class EncoderBlock(nn.Module):
                                                         dim_feedforward=dim_ff, dropout=0.1,
                                                         activation='relu', batch_first=True,
                                                         device=device)
+        self.encoder_layer3 = nn.TransformerEncoderLayer(d_model=embdim, nhead=num_heads,
+                                                        dim_feedforward=dim_ff, dropout=0.1,
+                                                        activation='relu', batch_first=True,
+                                                        device=device)
+        self.encoder_layer4 = nn.TransformerEncoderLayer(d_model=embdim, nhead=num_heads,
+                                                        dim_feedforward=dim_ff, dropout=0.1,
+                                                        activation='relu', batch_first=True,
+                                                        device=device)
         
     def forward(self, x):
         out = self.encoder_layer1(x)
         out = self.encoder_layer2(out)
+        out = self.encoder_layer3(out)
+        out = self.encoder_layer4(out)
         return out
 
 class TransfDec(nn.Module):
